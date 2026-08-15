b1f676f73cda912e6f079738698d857d7c8ee83a
superprey3-wq <super.prey3@gmail.com>
2026-08-15 15:47:01 +0300

    Enable non-blocking CORE entry sync in Docker runner

--- gool_bot/main.py
+++ gool_bot/main.py
@@ -18,6 +18,7 @@ import market_math_patch
 import gool_xg_consensus
 import telegram_signal_filter_patch
 import telegram_image_signal_patch
+import entry_sync_failopen_patch
 import core_result_card_patch
 import robust_goal_cooldown_patch
 import signal_journal_runtime_patch
