8d4a38e27131b375bb43cf9a4a83b541252a9ae4
superprey3-wq <super.prey3@gmail.com>
2026-08-25 21:10:21 +0300

    Bump build for auxiliary result minute fix

--- gool_bot/release_build_patch.py
+++ gool_bot/release_build_patch.py
@@ -1,4 +1,4 @@
 """Set a visible production build id after runtime patches are loaded."""
 import telegram_subscribers as ts
 
-ts.BUILD_ID = "GOOL-PROD-RESULTCARD-COOLDOWN-9"
+ts.BUILD_ID = "GOOL-PROD-AUX-MINUTE-10"
