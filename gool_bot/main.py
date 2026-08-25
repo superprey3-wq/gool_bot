29cfbeb4ea18cf7ddf28cc4fe77e97446def048a
superprey3-wq <super.prey3@gmail.com>
2026-08-25 21:24:32 +0300

    Load auxiliary score freshness guard in production

--- gool_bot/main.py
+++ gool_bot/main.py
@@ -47,6 +47,7 @@ import clv_tracker
 import live_status_heartbeat
 import fast_goal_watch
 import multi_engine_runtime
+import aux_score_freshness_patch
 import multi_source_aux_stats_patch
 import second_half_strategy_patch
 import card_explainability_patch
