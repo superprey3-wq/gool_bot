58248bd89261f7f0be7e7c4aa0ef9525f7473837
superprey3-wq <super.prey3@gmail.com>
2026-08-25 21:21:33 +0300

    Bump build for shots consistency fix

--- gool_bot/release_build_patch.py
+++ gool_bot/release_build_patch.py
@@ -1,4 +1,4 @@
 """Set a visible production build id after runtime patches are loaded."""
 import telegram_subscribers as ts
 
-ts.BUILD_ID = "GOOL-PROD-AUX-MINUTE-10"
+ts.BUILD_ID = "GOOL-PROD-SHOTS-SANITY-11"
