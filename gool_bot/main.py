7231340608890fa5c6857f7430397e2624393504
superprey3-wq <super.prey3@gmail.com>
2026-08-25 21:10:12 +0300

    Load auxiliary result minute fix in production

--- gool_bot/main.py
+++ gool_bot/main.py
@@ -51,6 +51,7 @@ import multi_source_aux_stats_patch
 import second_half_strategy_patch
 import card_explainability_patch
 import second_half_card_reason_patch
+import aux_result_minute_patch
 import release_build_patch
 from league_signal_gate import filter_for_multi_engine
 from telegram_subscribers import polling_loop,BUILD_ID
